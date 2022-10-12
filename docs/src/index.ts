5b84e103e0994be2a57ca1e23d0a865581853bca
kohki20220601 <fkoki20220601@gmail.com>
2022-10-12 23:41:14 +0900

    ace second

--- docs/src/index.ts
+++ docs/src/index.ts
@@ -1,6 +1,29 @@
 import { hello } from "./hello";
 import "./style.scss"
+import { header } from "./header";
+import $ from "jquery"
 
 const message:string = "こんにちはaaaaaa"
 
+header()
+
+// $(function(){
+//     console.log("i am index.ts on jquery")
+// })
+
+
+
 // hello(message)
+
+
+// typescriptちょっと練習。
+
+// const h1 = document.querySelector<HTMLElement>(`h1`)
+// console.log(h1)
+
+
+// if(h1){
+//     h1.onclick = function() {
+//         h1.setAttribute("class", "second")
+//     }
+// }
